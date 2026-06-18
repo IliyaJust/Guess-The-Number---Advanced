13f8afb81eeb3f6f7974d956579fd78eb1331b15
IliyaJust <akbariiliya521@gmail.com>
2026-06-18 16:03:22 +0330

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-
 import json
 import emoji
 class Game:
@@ -6,9 +5,9 @@ class Game:
         self.errors = []
         self.records = None
         self.challenges = None
-		self.achievements = None
-		self.unlocked_difficulties = []
-		self.coins = 0
+        self.achievements_data = None
+        self.unlocked_difficulties = []
+        self.coins = 0
         with open('firstinfo.json', 'r') as f:
             self.first_time = json.load(f)["First Time"]
         if self.first_time:
@@ -30,8 +29,8 @@ class Game:
                 json.dump(data, f, ensure_ascii=False, indent=4)
                 with open("firstinfo.json", 'w') as fi_f:
                     json.dump({"First Time": False}, fi_f, indent=2)
-			with open("achievements_data.json", 'w') as ad_d:
-				json.dump({}, ad_d, indent=2)
+            with open('achievements_data.json', 'w') as ad_f:
+                json.dump({}, ad_f, indent=2)
     def load_data(self):
         print(emoji.emojize(":mag: Loading data...", language='alias'))
         try:
@@ -66,15 +65,22 @@ class Game:
             self.errors.append("Challenges Data Is Corrupted")
         except FileNotFoundError:
             print(emoji.emojize(":cross_mark: Could not find challenges data!", language='alias'))
-            self.errors.append("Challenges Data File Not Found!")
-		try:
-			with open('achievements_data.json', 'r') as f:
-				achievements_data = json.load(f)
-			required_keys = ["Name", "Doc"]
-			for achievements, achievement_data in achievements_data.items():
-				for key in required_keys:
-					if key not in achievement_data:
-						self.errors.append(f"{achievement} has unknown data.Could not found {key}.Details: {achievement_data}"
+            self.errors.append("Challenges Data File Not Found")
+        try:
+            with open("achievements_data.json", 'r') as f:
+                achievements_data = json.load(f)
+            required_keys = ["Name", "Doc"]
+            for achievement, achievement_data in achievements_data.items():
+                for keys in required_keys:
+                    if key not in required_keys:
+                        self.errors.append(f"{achievement} has unknown data.Could not found {key}.Details: {achievement_data}")
+                        print(emoji.emojize(f":cross_mark: {achievement} has unknown data!", language = 'alias'))
+        except json.JSONDecodeError:
+            print(emoji.emojize(":cross_mark: Achievements data is corrupted!", language = 'alias'))
+            self.errors.append("Achievements Data Is Corrputed")
+        except FileNotFoundError:
+            print(emoji.emojize(":warning: It is like you don't have any achievements", language = 'alias'))
+            self.errors.append("Achievements Data File Not Found")
         if self.errors:
             print(emoji.emojize(":mag: There Is Some Errors! Are you sure you want to continue:question:", language='alias'))
             continue_question = input("[Details, No] (Default: No): ").lower()
@@ -108,12 +114,12 @@ Choose a difficulty or a option.
 --------
 Difficulties
 ----
-1)Easy(1-20) - 15 Guesses
-2)Normal(1-100) - 10 Guesses {"" if "normal" in self.unlocked_difficulties else "(Locked)"}
-3)Hard(1-1_000) - 10 Guesses {"" if "hard" in self.unlocked_difficulties else "(Locked)"}
-4)Expert(1-10_000) - 13 Guesses {"" if "expert" in self.unlocked_difficulties else "(Locked)"}
-5)Nightmare(1-100_000) - 10 Guesses {"" if "nightmare" in self.unlocked_difficulties else "(Locked)"}
-6)Impossible(1-1_000_000) - 12 Guesses {"" if "impossible" in self.unlocked_difficulties else "(Locked)"}
+1)Easy(1-20) - 15 Guesses - 5 Coins
+2)Normal(1-100) - 10 Guesses {"" if "normal" in self.unlocked_difficulties else "(Locked)"} - 10 Coins
+3)Hard(1-1_000) - 10 Guesses {"" if "hard" in self.unlocked_difficulties else "(Locked)"} - 20 Coins
+4)Expert(1-10_000) - 13 Guesses {"" if "expert" in self.unlocked_difficulties else "(Locked)"} - 40 Coins
+5)Nightmare(1-100_000) - 10 Guesses {"" if "nightmare" in self.unlocked_difficulties else "(Locked)"} - 80 Coins
+6)Impossible(1-1_000_000) - 12 Guesses {"" if "impossible" in self.unlocked_difficulties else "(Locked)"} - 160 Coins
 ------
 Options
 ----
@@ -122,5 +128,12 @@ Options
 9)Records
 10)Exit
 """)
+        choose = input("Please choose a number(1-10): ")
+        if choose == 1:
+            return "Easy"
+        elif choose == 2:
+            if "normal" not in self.unlocked_difficulties:
+                unlock = input("This difficulty is locked! Are you sure you want to unlock it[y,n]?(Win Easy, 50 Coins): ")
+                if unlock == "y":
    
         
