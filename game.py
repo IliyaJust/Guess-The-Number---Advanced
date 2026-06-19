db38392fb179d408c63c4151e8cef3da919b2c4a
IliyaJust <akbariiliya521@gmail.com>
2026-06-19 18:26:21 +0330

    Continue menu function.

--- game.py
+++ game.py
@@ -75,6 +75,7 @@ class Game:
                     if key not in required_keys:
                         self.errors.append(f"{achievement} has unknown data.Could not found {key}.Details: {achievement_data}")
                         print(emoji.emojize(f":cross_mark: {achievement} has unknown data!", language = 'alias'))
+            print(emoji.emojize(":white_check_mark: Achievements data loaded.", language = 'alias'))
         except json.JSONDecodeError:
             print(emoji.emojize(":cross_mark: Achievements data is corrupted!", language = 'alias'))
             self.errors.append("Achievements Data Is Corrputed")
@@ -106,7 +107,8 @@ class Game:
                 return False
         return True
     def menu(self):
-        print(f"""
+        while True:
+            print(f"""
 Guess The Number - Advanced | V Alpha 0.1
 {"=" * 50}
 Coins: {self.coins}
@@ -126,14 +128,24 @@ Options
 7)Challenges
 8)Achievements
 9)Records
-10)Exit
-""")
-        choose = input("Please choose a number(1-10): ")
-        if choose == 1:
-            return "Easy"
-        elif choose == 2:
-            if "normal" not in self.unlocked_difficulties:
-                unlock = input("This difficulty is locked! Are you sure you want to unlock it[y,n]?(Win Easy, 50 Coins): ")
-                if unlock == "y":
-                    pass #Developing...
+10)Update News
+11)Exit
+    """)
+            choose = input("Please choose a number(1-10): ")
+            if choose == "1":
+                return {"Easy": {"Guesses": 15, "Reward": 5}}
+            elif choose == "2":
+                if "normal" not in self.unlocked_difficulties:
+                    unlock = input(emoji.emojize(":warning: This difficulty is locked! Are you sure you want to unlock it[y,n]?(50 Coins): ", language = 'alias'))
+                    if unlock == "y":
+                        if self.coins < 50:
+                            print(emoji.emojize(":coin: Your coins aren't enough to buy Normal difficulty!"))
+                            input("Press enter to continue...")
+                            continue
+                        self.unlocked_difficulties.append("normal")
+                        print(emoji.emojize(":with_check_mark: Successfully buyed Normal difficulty!"))
+                        input("Press enter to continue...")
+                        continue
+            elif choose == "3":
+                pass
         
